85a5d02c835a3df68dbbc59d9347ce505c208fd1
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-13 02:15:43 +0200

    Implement player spacemod interaction

--- game/player/Player.gd
+++ game/player/Player.gd
@@ -12,10 +12,16 @@ var direction := Vector3()
 var smoothed_direction := Vector3()
 var desired := Transform()
 var possible_pathways := []
+var possible_spacemods := []
 var offset := 0.0
 var has_next := false
 var next: Curve3D
+var next_pathway: Pathway
 var current: Curve3D
+var current_pathway: Pathway
+var up := Vector3.UP
+
+var active_spacemod  # No type to make this nullable
 
 onready var path_detector: Area = $PathDetector
 
@@ -23,12 +29,13 @@ onready var path_detector: Area = $PathDetector
 func _ready() -> void:
     
     # warning-ignore:return_value_discarded
-    path_detector.connect('area_entered', self, '_on_pathway_entered')
+    path_detector.connect('area_entered', self, '_on_area_entered')
     # warning-ignore:return_value_discarded
-    path_detector.connect('area_exited', self, '_on_pathway_exited')
+    path_detector.connect('area_exited', self, '_on_area_exited')
     
     desired = global_transform
-    current = get_node(first_path).get_global_curve()
+    current_pathway = get_node(first_path)
+    current = current_pathway.get_global_curve()
     offset = 0.0
     var ahead := _interpolate_offset(offset + lookahead, Vector3.UP)
     var start := _interpolate_offset(offset, Vector3.UP, true)
@@ -53,8 +60,10 @@ func _process(delta: float) -> void:
     offset += delta * speed
     var ahead := _interpolate_offset(offset + lookahead, direction)
     var start := _interpolate_offset(offset, direction, true)
-    var up := Vector3.UP
     var pos = start + ((ahead - start) * 0.5)
+    
+    if active_spacemod:
+        up = active_spacemod.get_up_vector(global_transform, up)
     
     desired.origin = pos + smoothed_direction
     desired = desired.looking_at(ahead + smoothed_direction, up)
@@ -73,13 +82,16 @@ func _interpolate_offset(offs: float, dir: Vector3, make_current := false) -> Ve
                 push_error('Path ended without any possible continuations!')
                 return current.interpolate_baked(current.get_baked_length())
             # TODO: Use actual logic to determine which path to choose
-            next = _get_best_pathway(dir).get_global_curve()
+            next_pathway = _get_best_pathway(dir)
+            next = next_pathway.get_global_curve()
             has_next = true
             possible_pathways.clear()
         if make_current:
+            current_pathway = next_pathway
             current = next
             offset = remaining
             has_next = false
+            _recheck_possible_spacemods()
         return next.interpolate_baked(remaining)
     else:
         return current.interpolate_baked(offs)
@@ -96,13 +108,56 @@ func _get_best_pathway(dir: Vector3) -> Pathway:
     return best
 
 
-func _on_pathway_entered(other: Area):
+func _recheck_possible_spacemods() -> void:
+    var idx = 0
+    while idx < possible_spacemods.size():
+        var mod = possible_spacemods[idx]
+        if mod.curve_matches(current_pathway):
+            _set_spacemod(mod)
+            return
+        idx += 1
+
+
+func _set_spacemod(mod: SpaceMod) -> void:
+    if active_spacemod:
+        push_warning('Forced spacemode replacement: ' + mod.name)
+        if active_spacemod.is_connected('finished', self, '_unset_spacemod'):
+            active_spacemod.disconnect('finished', self, '_unset_spacemod')
+    active_spacemod = mod
+    active_spacemod.activate()
+    active_spacemod.connect('finished', self, '_unset_spacemod', [], CONNECT_ONESHOT)
+
+
+func _unset_spacemod() -> void:
+    active_spacemod = null
+
+
+func _on_area_entered(other: Area) -> void:
+    
+    # Check for space mods
+    if other is SpaceMod:
+        if other.curve_matches(current_pathway):
+            _set_spacemod(other)
+        else:
+            possible_spacemods.append(other)
+    
+    # Check for pathways
     var parent = other.get_parent()
     if parent is Pathway:
         possible_pathways.append(parent)
 
 
-func _on_pathway_exited(other: Area):
+func _on_area_exited(other: Area) -> void:
+    
+    # Notify active spacemod
+    if other == active_spacemod:
+        active_spacemod.on_player_left_area()
+    
+    # Remove possible space mods
+    while other in possible_spacemods:
+        possible_spacemods.erase(other)
+    
+    # Remove possible pathways
     var parent = other.get_parent()
     while parent in possible_pathways:
         possible_pathways.erase(parent)
