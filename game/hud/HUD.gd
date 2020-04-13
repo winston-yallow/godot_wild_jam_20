c16370b1a613e480dd80bcc3769ac179e0145dd7
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-13 21:07:05 +0200

    Add health display to HUD

--- game/hud/HUD.gd
+++ game/hud/HUD.gd
@@ -4,8 +4,11 @@ extends Control
 export var player_path: NodePath = '.'
 export var interpolation = 30.0
 export var direction_radius := 40
+export var health_offset := Vector2(0, 60)
 
 var player: Player
+var health: Label
+var healthbar_rect: Rect2
 var direction := Vector2()
 var smoothed_direction := Vector2()
 var direction_interpolated := Vector2()
@@ -13,6 +16,7 @@ var direction_interpolated := Vector2()
 
 func _ready() -> void:
     player = get_node(player_path)
+    health = $Health
 
 
 func _process(delta: float) -> void:
@@ -27,11 +31,18 @@ func _process(delta: float) -> void:
 
 
 func _draw() -> void:
+    
     var cam := get_tree().root.get_camera()
     var center := cam.unproject_position(player.to_global(player.nose))
+    
+    # Draw direction indicators
     draw_arc(center, direction_radius, 0, TAU, 24, Color(0, 1.0, 0.7, 0.6), 1.0)
     draw_circle(center + smoothed_direction, 4, Color(0, 1.0, 0.7, 0.6))
     draw_circle(center + direction_interpolated, 2, Color(0, 1.0, 0.7, 0.9))
+    
+    # Configure health text label
+    health.text = str(round(player.health)) + '%'
+    health.rect_global_position = center - (health.rect_size / 2) + health_offset
 
 
 func _to_vec2(v: Vector3, flip_y := true) -> Vector2:
