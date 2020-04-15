34137074ed322109bf449976c212fb37da5bf7d0
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-15 12:10:55 +0200

    Make thinking laser shoot in bursts

--- game/weapons/laser_gun/ThinkingLaser.gd
+++ game/weapons/laser_gun/ThinkingLaser.gd
@@ -3,11 +3,13 @@ extends Area
 
 export var laser_bullet_speed := 20.0
 export var smoothness := 1.5
+export var cooldown := 0.23
 
 var active := false
 var player: Player
 var interpolated_velocity := Vector3()
 
+onready var next_recalc := cooldown
 onready var gun := $LaserGun
 onready var target := $Target
 
@@ -28,13 +30,21 @@ func _process(delta: float) -> void:
     interpolated_velocity = interpolated_velocity.linear_interpolate(
         player.velocity, delta * smoothness
     )
-    if active:
-        var dist := (player.global_transform.origin - global_transform.origin).length()
-        var traveltime = dist / laser_bullet_speed
-        var offset = interpolated_velocity * traveltime
-        target.global_transform.origin = player.global_transform.origin + offset
-    else:
+    
+    if not active:
         target.transform.origin = Vector3.FORWARD
+    elif next_recalc <= 0:
+        _recalc_target_pos()
+        next_recalc = cooldown
+    else:
+        next_recalc -= delta
+
+
+func _recalc_target_pos() -> void:
+    var dist := (player.global_transform.origin - global_transform.origin).length()
+    var traveltime = dist / laser_bullet_speed
+    var offset = interpolated_velocity * (traveltime + (cooldown * 0.75))
+    target.global_transform.origin = player.global_transform.origin + offset
 
 
 func _on_body_entered(other: Spatial) -> void:
