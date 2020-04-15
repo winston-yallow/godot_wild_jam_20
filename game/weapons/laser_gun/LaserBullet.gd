84b225ab39fd0b7afe79eb2fb2a2980f1dbd0e94
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-15 13:51:13 +0200

    Make bullets spatials & use raycasts for collision

--- game/weapons/laser_gun/LaserBullet.gd
+++ game/weapons/laser_gun/LaserBullet.gd
@@ -1,28 +1,38 @@
-extends Area
+extends Spatial
 
 
-var harm = 0.2
-var speed = 20
-var lifetime = 2.0
+export(int, FLAGS, 'Player', 'Enemies', 'Obstacles') var collision_mask = 7
+
+var harm := 0.2
+var speed := 20
+var lifetime := 2.0
 var exclude := []
+var lookahead := 1.5
 
 
-func _ready() -> void:
-    # warning-ignore:return_value_discarded
-    connect('body_entered', self, '_on_body_entered')
-
-
-func _process(delta: float) -> void:
-    translate(Vector3.FORWARD * speed * delta)
+func _physics_process(delta: float) -> void:
+    
+    var movement := -global_transform.basis.z * speed * delta
     lifetime -= delta
     if lifetime < 0:
         queue_free()
-
-
-func _on_body_entered(other: PhysicsBody) -> void:
-    if other.has_method('damage') and not other in exclude:
-        other.damage(harm)
-    queue_free()
+    
+    var space_state := get_world().direct_space_state
+    var result := space_state.intersect_ray(
+        global_transform.origin,
+        global_transform.origin + (movement * lookahead),
+        exclude,
+        collision_mask,
+        true,
+        false
+    )
+    
+    if result:
+        if result.collider.has_method('damage'):
+            result.collider.damage(harm)
+        queue_free()
+    
+    global_transform.origin += movement
 
 func set_material(mat: Material):
     $Mesh.material_override = mat
