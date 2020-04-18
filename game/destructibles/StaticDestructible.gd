6ccf5f9bd9008a6b6e7511f3671243351531162f
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-18 15:33:14 +0200

    Make destructibles explode when destroyed

--- game/destructibles/StaticDestructible.gd
+++ game/destructibles/StaticDestructible.gd
@@ -4,6 +4,8 @@ class_name StaticDestructible
 
 signal destroyed
 
+const Explosion := preload('res://effects/Explosion.tscn')
+
 export var health := 100.0
 
 
@@ -11,4 +13,7 @@ func damage(amount: float) -> void:
     health -= amount
     if health <= 0:
         emit_signal('destroyed')
+        var explosion := Explosion.instance()
+        get_tree().current_scene.add_child(explosion)
+        explosion.global_transform = global_transform
         queue_free()
