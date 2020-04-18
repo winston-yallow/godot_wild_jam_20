6b6d675c127dfd1f095ccd561f90491f760c75f9
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-18 18:11:48 +0200

    Add start animation

--- game/hud/HUD.gd
+++ game/hud/HUD.gd
@@ -5,10 +5,15 @@ export var player_path: NodePath = '.'
 export var interpolation = 30.0
 export var direction_radius := 40
 export var health_offset := Vector2(0, 60)
+export var intro_time := 2.5
+export var intro_fade := 0.3
 
 var player: Player
 var health: Label
 var healthbar_rect: Rect2
+var audio: AudioStreamPlayer
+var intro_node: Control
+var intro_remaining: float
 var direction := Vector2()
 var smoothed_direction := Vector2()
 var direction_interpolated := Vector2()
@@ -16,10 +21,26 @@ var direction_interpolated := Vector2()
 
 func _ready() -> void:
     player = get_node(player_path)
+    # warning-ignore:return_value_discarded
+    player.connect('restarted', self, '_on_player_restart')
+    
+    intro_remaining = intro_time
+    intro_node = $Intro
+    
+    audio = $AudioStreamPlayer
+    audio.play(0.0)
+    
     health = $Health
 
 
 func _process(delta: float) -> void:
+    
+    if intro_remaining > 0:
+        intro_remaining -= delta
+        intro_node.modulate.a = clamp(intro_remaining / intro_fade, 0, 1)
+    elif intro_node.visible:
+        intro_node.visible = false
+    
     var direction3d := player.get_local_direction()
     direction = _to_vec2(direction3d) * direction_radius
     var smoothed_direction3d := player.get_local_smoothed_direction()
@@ -43,6 +64,12 @@ func _draw() -> void:
     # Configure health text label
     health.text = str(round(player.health)) + '%'
     health.rect_global_position = center - (health.rect_size / 2) + health_offset
+
+
+func _on_player_restart():
+    intro_remaining = intro_time
+    intro_node.visible = true
+    audio.play(0.0)
 
 
 func _to_vec2(v: Vector3, flip_y := true) -> Vector2:
