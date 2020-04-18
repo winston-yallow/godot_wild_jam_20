d84dc643955ea9e5a8837c86cbce6e76bac457b7
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-18 14:19:09 +0200

    Add interpolated controllable camera

---
+++ game/player/Cam.gd
@@ -0,0 +1,36 @@
+extends Spatial
+
+
+export var speed := 8.0
+export var target_path := NodePath('.')
+export var max_angle := 0.2
+
+var target: Spatial
+var pivot: Position3D
+
+
+func _ready() -> void:
+    set_as_toplevel(true)
+    target = get_node(target_path)
+    pivot = $Pivot
+
+
+func _process(delta: float) -> void:
+    
+    transform = transform.interpolate_with(
+        target.global_transform,
+        delta * speed
+    )
+    
+    var look := Vector3()
+    look.y += Input.get_action_strength('look_left')
+    look.y -= Input.get_action_strength('look_right')
+    look.x += Input.get_action_strength('look_up')
+    look.x -= Input.get_action_strength('look_down')
+    
+    if look.length() > 1:
+        look = look.normalized()
+    
+    look *= max_angle
+    
+    pivot.rotation = pivot.rotation.linear_interpolate(look, delta * speed)
