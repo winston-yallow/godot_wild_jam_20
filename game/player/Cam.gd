c9e8805c5c75c0be0be8f840ff17f86b7960b3f1
Winston Yallow <44872771+winston-yallow@users.noreply.github.com>
2020-04-18 16:47:45 +0200

    Add mouse support

--- game/player/Cam.gd
+++ game/player/Cam.gd
@@ -3,16 +3,32 @@ extends Spatial
 
 export var speed := 8.0
 export var target_path := NodePath('.')
-export var max_angle := 0.5
+export var max_angle := 0.7
+export var mouse_threshold := 0.3
+export var mouse_sensitivity := 0.01
+
+var mouse_preferred := true
+var mouse_axis := Vector3()
 
 var target: Spatial
 var pivot: Position3D
 
 
 func _ready() -> void:
+    Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
     set_as_toplevel(true)
     target = get_node(target_path)
     pivot = $Pivot
+
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseMotion:
+        var mouse_vector = event.relative * mouse_sensitivity
+        mouse_axis -= Vector3(mouse_vector.y, mouse_vector.x, 0)
+        if not mouse_preferred and mouse_axis.length() > mouse_threshold:
+            mouse_preferred = true
+        if mouse_axis.length() > 1:
+            mouse_axis = mouse_axis.normalized()
 
 
 func _process(delta: float) -> void:
@@ -28,7 +44,12 @@ func _process(delta: float) -> void:
     look.x += Input.get_action_strength('look_up')
     look.x -= Input.get_action_strength('look_down')
     
-    if look.length() > 1:
+    if look.length() > 0:
+        mouse_preferred = false
+    
+    if mouse_preferred:
+        look = mouse_axis
+    elif look.length() > 1:
         look = look.normalized()
     
     look *= max_angle
